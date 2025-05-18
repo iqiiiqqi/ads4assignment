ce2122271364400386e4553c6ff45eb7bc7d340e
iqiiiqqi <sabinasain35@gmail.com>
2025-05-18 20:24:55 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -25,33 +25,6 @@ public class Main {
         outputPath(bfs, "Kyzylorda");
     }
 
-   public class Main {
-
-    public static void main(String[] args) {
-        WeightedGraph<String> weightedGraph = new WeightedGraph<>(true);
-        fillWithWeights(weightedGraph);
-
-        System.out.println("Dijkstra:");
-        Search<String> djk = new DijkstraSearch<>(weightedGraph, "Almaty");
-        outputPath(djk, "Kyzylorda");
-
-
-        System.out.println("--------------------------------");
-
-        UnweightedGraph<String> graph = new UnweightedGraph<>(true);
-        fillWithoutWeights(graph);
-
-        System.out.println("DFS:");
-        Search<String> dfs = new DepthFirstSearch<>(graph, "Almaty");
-        outputPath(dfs, "Kyzylorda");
-
-        System.out.println("--------------------------------");
-
-        System.out.println("BFS:");
-        Search<String> bfs = new BreadthFirstSearch<>(graph, "Almaty");
-        outputPath(bfs, "Kyzylorda");
-    }
-
     public static void fillWithoutWeights(UnweightedGraph<String> graph) {
         graph.addEdge("Almaty", "Astana");
         graph.addEdge("Astana", "Atyrau");
@@ -61,25 +34,9 @@ public class Main {
         graph.addEdge("Shymkent", "Astana");
         graph.addEdge("Astana", "Kostanay");
     }
-       
-    public static void fillWithWeights(WeightedGraph<String> graph) {
-        graph.addEdge("Almaty", "Astana", 2.1);
-        graph.addEdge("Shymkent", "Atyrau", 7.8);
-        graph.addEdge("Atyrau", "Astana", 7.1);
-        graph.addEdge("Almaty", "Shymkent", 7.2);
-        graph.addEdge("Shymkent", "Astana", 3.9);
-        graph.addEdge("Astana", "Kostanay", 3.5);
-        graph.addEdge("Shymkent", "Kyzylorda", 5.4);
-    }
 
-    public static void outputPath(Search<String> search, String key) {
-        for (String v : search.pathTo(key)) {
-            System.out.print(v + " -> ");
-        }
 
-        System.out.println();
-    }
-}
+
     public static void fillWithWeights(WeightedGraph<String> graph) {
         graph.addEdge("Almaty", "Astana", 2.1);
         graph.addEdge("Shymkent", "Atyrau", 7.8);
